dcac051c1d371ac16bc85a1e914b5a13c9591473
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-31 16:10:51 +0200

    Initial Commit

---
+++ SwiftUISocialMediaApp_UIKit&SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISocialMediaApp_UIKit&SwiftUI
+//
+//  Created by mohamed  habib on 31/05/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
