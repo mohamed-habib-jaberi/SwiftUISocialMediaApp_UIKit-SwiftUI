78046d49ec87df45d38df7742434a69ccba841bd
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-05-31 23:02:05 +0200

    Add Toggle to Filter post if loved

--- SwiftUISocialMediaApp_UIKit&SwiftUI/Mews/Master/MewsListView.swift
+++ SwiftUISocialMediaApp_UIKit&SwiftUI/Mews/Master/MewsListView.swift
@@ -11,13 +11,21 @@ import SwiftUI
 struct MewsListView: View {
     
     @State private var posts: [MewsPost] = MewsPost.demoPosts
+    @State private var isFilteringLovedPosts: Bool = false
     
     var body: some View {
         NavigationView{
             List{
                
+                Toggle(isOn: $isFilteringLovedPosts) {
+                    Text(verbatim: "Show loved posts only")
+                }
+                
                 ForEach(posts) { post in
-                    MewsRow(post: post)
+                    if !self.isFilteringLovedPosts || post.reaction == .love {
+                          MewsRow(post: post)
+                    }
+                  
                 }
             }
         .navigationBarTitle(Text(verbatim: "Cat Mews"))
